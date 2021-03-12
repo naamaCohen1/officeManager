e646fbe2611f29f8eab7825f13b7d593d95521e5
naama <naama@NAAMA-DELL>
2021-03-12 16:34:18 +0200

    testing

--- officeManager/Startup.cs
+++ officeManager/Startup.cs
@@ -17,7 +17,7 @@ namespace officeManager
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container. gfgfd
         public void ConfigureServices(IServiceCollection services)
         {
 
