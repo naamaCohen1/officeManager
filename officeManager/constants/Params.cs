c2353d5eb135af950d8bd8a3f1fa301c6d5823ca
cteveth <cteveth@gmail.com>
2021-08-08 16:35:38 +0300

    connection string

--- officeManager/constants/Params.cs
+++ officeManager/constants/Params.cs
@@ -8,8 +8,8 @@ namespace officeManager.constants
     public class Params
     {
         
-        public static readonly string connetionString = @"Data Source=DESKTOP-U9FO5L4,1433;Initial Catalog=OfficeManagerDB;User ID=naama;Password=naama";
-        //public static readonly string connetionString = @"Data Source=NAAMA-DELL;Initial Catalog=OfficeManagerDB;Integrated Security=SSPI";
+        //public static readonly string connetionString = @"Data Source=DESKTOP-U9FO5L4,1433;Initial Catalog=OfficeManagerDB;User ID=naama;Password=naama";
+        public static readonly string connetionString = @"Data Source=NAAMA-DELL;Initial Catalog=OfficeManagerDB;Integrated Security=SSPI";
 
     }
 }
